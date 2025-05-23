591cfd12b0bc16917111404b63070c9bec9eac9c
Mel <melanyvbustamante@gmail.com>
2025-05-22 23:50:50 -0500

    Faltan validaciones

---
+++ BaseDatos.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class BaseDatos {
+    public static ArrayList<EstudianteIngenieria> ingenieros = new ArrayList<>();
+    public static ArrayList<EstudianteDiseño> diseñadores = new ArrayList<>();
+    public static ArrayList<ComputadorPortatil> portatiles = new ArrayList<>();
+    public static ArrayList<TabletaGrafica> graficas = new ArrayList<>();
+}
